8f8ed9d45a7ed900f80a1628d323a666e64c80d2
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2022-06-24 01:19:17 +1000

    (More 'Created Project' commits)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import HomeScreen from './screens/HomeScreen';
+
+type StackParamList = {
+  HomeScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+/*     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{title: 'MERN Mafia!'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
